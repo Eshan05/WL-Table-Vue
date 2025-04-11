6bf98e0306c4edc6afac05b48d5ef94c1e23deb7
Red X <Eshan05>
2025-04-11 16:00:03 +0530

    Big bang

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+<template>
+  <main>
+    <span class="font-mono">Jet brains =></span> <span>Geist?</span>
+  </main>
+</template>
+<style scoped>
+
+</style>
